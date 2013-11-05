65d364cbf22786fc769d6ee4437cacd987e5db58
watanabe <alcange@hotmail.co.jp>
2013-11-05 16:04:27 +0900

    足し算アプリの作成（学習用）

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdditionApp::Application.config.secret_key_base = '84d16aae98f8d426f87da5f48dbe6498e5f403b25a6ee4426028c026b6b7cf063a5cad4b2c20471c98ff6cbb2273911b82221c01238a4122f7825d098dbb958d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdditionApp::Application.config.session_store :cookie_store, key: '_addition_app_session'
